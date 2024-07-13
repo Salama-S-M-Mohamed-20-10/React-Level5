4f369355b66e2acdfd260c10305ea69edfe6a7b0
Salama-S-M-Mohamed-20-10 <Salama.MMohamed10@gmail.com>
2024-07-13 12:44:01 +0300

    lesson5

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,35 +1,108 @@
-import { Box, Button, InputAdornment, TextField, styled } from "@mui/material";
+import { Badge, Box, Button, Divider, IconButton, InputAdornment, Paper, Stack, TextField, Typography, styled } from "@mui/material";
 import "./Cart.css";
+import { Add, Delete, Remove } from "@mui/icons-material";
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: "#1976d2",
+    color: "#fff"
+  },
+}));
 
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-//import { useTheme } from "@mui/material/styles";
 
 const Cart = () => {
 
-  //const theme = useTheme();
   return (
     <Box >
-            Cart Page
+            
+
+
+            <Paper dir="rtl" className="item-container">
+
+            <div className="img-title-parent">
+                <img src="####" alt="" />
+                <p className="product-name">T-shirt</p>
+            </div>
+
+
+
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <IconButton sx={{color:"#1976d2", ml: "10px"}} onClick={() => {
+
+                }}>
+
+                  
+                  <Add />
+                  
+                </IconButton>
+                
+
+                <StyledBadge badgeContent={1} color="secondary" />
+
+                <IconButton sx={{color:"#1976d2", mr: "10px"}} onClick={() => {
+
+                    }}>
+
+                      
+                  <Remove /> 
+                </IconButton>
+
+            </div>
+
+            <div className="price">
+                $100
+            </div>
+
+
+            <Button sx={{ display: {xs: "none", md: "inline-flex"} }}
+            variant="text" color="error">
+                delete
+            </Button>
+
+            <IconButton sx={{ color: "#ef5350", display: {xs: "inline-flex", md: "none"} }} onClick={() => {
+              
+               }
+            }>
+              <Delete />
+            </IconButton>
+
+
+
+
+    </Paper>
+
+
+
+
+    <Paper sx={{ width: "200px", mx:"auto", mt:"60px" }}>
+
+          <Typography align="center" p={2} variant="h6">
+               Cart Summary
+          </Typography>
+
+          <Divider />
+
+          <Stack sx={{ justifyContent: "space-between", p: 1.2 }} direction={"row"}>
+
+               <Typography variant="body1">Subtotal</Typography>
+               <Typography variant="body1">$100</Typography>
+
+          </Stack>
+
+
+          <Divider />
+
+          <Button fullWidth color="primary" variant="contained">
+              CHECKOUT
+          </Button>
+
+    </Paper>
 
 
         
 
 
-    </Box>
+    </Box> 
   );
 };
 
